d94c549f200f9ba17c7918532d080baa177bd70f
KHTangent <contact@khtangent.com>
2023-02-09 12:42:36 +0100

    Update plotting script for new format

--- scripts/latencyplotter.py
+++ scripts/latencyplotter.py
@@ -19,32 +19,32 @@ if len(argv) < 2:
 filename = argv[1]
 
 with open(filename, "rb") as file:
-	x = []
-	y = []
+	transmit_times = []
+	latencies = []
 	while True:
 		try:
 			# The timestamp file contains raw doubles, where every other value is arrival time 
 			# and measured latency
+			raw_transmit = file.read(8)
 			raw_arrival = file.read(8)
-			raw_latency = file.read(8)
+			transmit = struct.unpack("d", raw_transmit)[0]
 			arrival = struct.unpack("d", raw_arrival)[0]
-			latency = struct.unpack("d", raw_latency)[0]
-			x.append(arrival)
-			y.append(latency)
+			transmit_times.append(transmit)
+			latencies.append(arrival-transmit)
 		except:
 			break
 
-total_duration = round(x[-1] - x[0])
-one_second_length = len(x)//total_duration
-average_latency = sum(y)/len(y)
-variance = sum(map(lambda e: (e-average_latency)**2, y))/len(y)
+total_duration = round(transmit_times[-1] - transmit_times[0])
+one_second_length = len(transmit_times)//total_duration
+average_latency = sum(latencies)/len(latencies)
+variance = sum(map(lambda e: (e-average_latency)**2, latencies))/len(latencies)
 standard_deviation = sqrt(variance)
 
 print(f"""
   Summary
 ===========
       Test duration (s):  {total_duration}
-      Number of packets:  {len(y)}
+      Number of packets:  {len(latencies)}
     Average latency (s):  {average_latency}
    Average latency (µs):  {average_latency * 10**6}
  Standard deviation (s):  {standard_deviation}
@@ -55,17 +55,19 @@ if len(argv) > 2 and argv[2] == "s":
 	exit()
 
 # The arrival time is relative to something, simply set the arrival time of the first packet as 0
-x_adjusted = list(map(lambda e: e-x[0], x))
-# Values are in seconds, scale to milliseconds
-y_scaled = list(map(lambda e: e * 10**6, y))
+x = list(map(lambda e: e-transmit_times[0], transmit_times))
+# Values are in seconds, scale to microseconds
+y = list(map(lambda e: e * 10**6, latencies))
+# y = [(latencies[i]-latencies[i-1])*10**6 for i in range(1, len(transmit_times))]
+# y.append(0)
 
 plt.style.use("dark_background")
 if len(argv) > 2 and argv[2] == "1":
 	first_index = (total_duration // 2 - 1) * one_second_length
 	last_index = first_index + one_second_length
-	plt.plot(x_adjusted[first_index:last_index], y_scaled[first_index:last_index], "go", markersize=0.2)
+	plt.plot(x[first_index:last_index], y[first_index:last_index], "go", markersize=0.2)
 else:
-	plt.plot(x_adjusted, y_scaled, "go", markersize=0.2)
+	plt.plot(x, y, "go", markersize=0.2)
 plt.xlabel("Transmit time (s)")
 plt.ylabel("Latency (µs)")
 
